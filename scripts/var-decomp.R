cf34f4942021c3e94e951e681ebb56ccd5646d08
Daniel Anderson <daniela@uoregon.edu>
2019-09-23 17:08:29 -0700

    added year and cohort to the decomposition

--- scripts/var-decomp.R
+++ scripts/var-decomp.R
@@ -8,37 +8,62 @@ gaps <- read_csv(here("data", "achievement-gaps-geocoded.csv"))
 gaps_model_d <- gaps %>%
 	mutate(state = relevel(factor(state), ref = "CA"),
 		     content = relevel(factor(content), ref = "ELA"))
-
+  
 base_var_hisp <- lmer(v_hisp ~ content + grade +
-                   (1|school_id) + (1|district_id) + (1|cnty) + (1|state), 
+                        (1|school_id) + (1|district_id) + (1|cnty) + 
+                        (1|year) + (1|cohort) + (1|state), 
                  gaps_model_d)
+# failed to converge
+
+all_fits_hisp <- allFit(base_var_hisp)
+summary(all_fits_hisp)
+
 summary(base_var_hisp)
+# Nelder_Mead, nlminbwrap, and nmkbw all converge fine. Models look 
+# functionally similar though
+
+base_var_hisp <- update(base_var_hisp, 
+                        control = lmerControl(optimizer = "nmkbw"))
 
 base_var_econ <- lmer(v_econ ~ content + grade +
-                   (1|school_id) + (1|district_id) + (1|cnty) + (1|state), 
+                        (1|school_id) + (1|district_id) + (1|cnty) + 
+                        (1|year) + (1|cohort) + (1|state),  
                  gaps_model_d)
-summary(base_var_econ)
 
+all_fits_econ <- allFit(base_var_econ)
+summary(all_fits_econ)
+# nmkbw is okay, all others are not. Again, estimates largely look identical, 
+# although Nelder mead is a bit different
+
+base_var_econ <- update(base_var_econ, 
+                        control = lmerControl(optimizer = "nmkbw"))
+
+summary(base_var_econ)
 
 extract_var <- function(mod) {
 	as.data.frame(VarCorr(mod)) %>%
 		select(component = grp, variance = vcov) %>%
 		mutate(proportion = variance / sum(variance))	
 }
-vars <- map_df(list("Hispanic/White" = base_var_hisp, 
+vars_raw <- map_df(list("Hispanic/White" = base_var_hisp, 
 										"Economic Disadvantage/All Students" = base_var_econ), 
 	     				 extract_var, 
 	     				 .id = "model")
 
-lbls <- c("Schools", "Districts", "Counties", "States", "Residual")
+lbls <- c("Schools", "Districts", "Counties", "Psuedo-Cohorts", 
+          "Academic Year", "States", "Residual")
 
 better_labels <- tibble(label = factor(lbls, levels = lbls),
-	                      component = unique(vars$component)) 
+	                      component = unique(vars_raw$component)) 
 
-vars <- left_join(vars, better_labels)
+vars <- left_join(vars_raw, better_labels) %>% 
+  filter(label != "Psuedo-Cohorts",
+         label != "Academic Year")
 
 label_locs <- vars %>%
-	filter(model == "Hispanic/White") %>%
+	filter(model == "Hispanic/White",
+	       label != "Psuedo-Cohorts",
+	       label != "Academic Year") %>%
 	arrange(desc(label)) %>%
 	mutate(cumsum = cumsum(proportion),
 			   cumsum_lag = lag(cumsum),
@@ -50,6 +75,11 @@ perc_label_locs <- vars %>%
 	arrange(desc(label)) %>%
 	mutate(lag = ifelse(is.na(lag(proportion)), 0, lag(proportion)),
 		     cumsum = cumsum(lag))
+vars_raw %>% 
+  filter(component == "cohort" |
+         component == "year") %>% 
+  group_by(model) %>% 
+  summarize(total_prop = sum(proportion)*100)
 
 ggplot(vars, aes(model, proportion)) +
 	geom_col(aes(fill = label),
@@ -68,7 +98,8 @@ ggplot(vars, aes(model, proportion)) +
 	scale_y_continuous(labels = scales::percent) +
 	guides(fill = "none") +
 	labs(x = "Achievement Gap",
-		   y = "Percentage of Total Variability") +
+		   y = "Percentage of Total Variability",
+		   caption = "Variance associated with pseudo-cohorts and years omitted, which combined accounted for approximately 0.05% of the total variance in each model") +
 	colorblindr::scale_fill_OkabeIto() +
 	theme_minimal(15) 
 
