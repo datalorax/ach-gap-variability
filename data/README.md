0987814fc1fc51a00d137660b62c2a683bfb3f27
Daniel Anderson <daniela@uoregon.edu>
2019-09-23 17:08:08 -0700

    Included additional years of data

--- data/README.md
+++ data/README.md
@@ -1,24 +1,28 @@
 # Achievement gap data
 
-The `achievement-gaps-geocoded.csv` file in this folder was used for all analyses in the paper. Below is a brief description of the data fields.
-The `scripts/dataprep.R` provides all the code used to create the file. 
-See more information on the files that went into this file below.
+The `achievement-gaps-geocoded.csv` file in this folder was used for all analyses in the paper. This includes achievement gap data for all schools reporting the percent proficient data on students coded as Hispanic and White OR Economically Disadvantaged and All Students across California, Oregon, and Washington from the 2014-15 to 2017-18 school years. Below is a brief description of the data fields. 
+
+The `scripts/dataprep.R` script provides all the code used to create the file. 
 
 ## Data fields
 
 | Column Name | Description|
 |:------------|:-----------|
+|`year`| The academic school year. One of "1415", "1516", "1617", or "1718" |
 |`state`| The corresponding state. One of `"CA"`, `"OR"` or `"WA"`|
 |`district_id`| Numeric identifier for the district|
 |`school_id`| Numeric identifier for the school|
 |`ncessch`| [NCES](https://nces.ed.gov) school identifier |
+|`content`| Test area. One of `"ELA"` for English/Language Arts or "Math" |
+|`grade`| Grade level|
+|`lat`| Latitude of the school|
+|`lon`| Longitude of the school|
 |`city`| City in which the school is or was located |
 |`cnty`| County code in which the school is or was located |
 |`nmcnty`| County name in which the school is or was located |
-|`lat`| Latitude of the school|
-|`lon`| Longitude of the school|
-|`content`| Test area. One of `"ELA"` for English/Language Arts or "Math" |
-|`grade`| Grade level|
+|`cbsatype`| Type of core based statistical area. One of "Metropolitan", "Micropolitan", or "Non-CBSA" |
+|`cbsa` | Core based statistical area |
+|`nmcbsa`| Name of the CBSA|
 |`n`| The total number of students in the grade level
 |`v_hisp`| Hispanic/White achievement gap estimate, interpreted in standard deviation units |
 |`v_econ`| Economically Disadvantaged/All Students achievement gap estimate, interpreted in standard deviation units |
@@ -34,26 +38,50 @@ Below is a description of the source files used to produce the `achievement-gaps
 |:----:|:-----|
 |  CA  | https://caaspp.cde.ca.gov/sb2018/ResearchFileList |
 |  OR  | https://www.oregon.gov/ode/educator-resources/assessment/Pages/Assessment-Group-Reports.aspx |
-|  WA  | http://reportcard.ospi.k12.wa.us/DataDownload.aspx |
+|  WA  | https://www.k12.wa.us/data-reporting/data-portal |
 
 **Geographic data retrieved from**
 
-* CCD: https://nces.ed.gov/ccd/Data/zip/ccd_sch_029_1718_w_0a_03302018_csv.zip
-* EDGE: https://nces.ed.gov/programs/edge/data/EDGE_GEOCODE_PUBLICSCH_1617.zip
+* CCD: https://nces.ed.gov/ccd/pubschuniv.asp
+* EDGE: https://nces.ed.gov/programs/edge/Geographic/SchoolLocations
 
 **The specific names of the files used in these analyses are**
 * California
+	+ sb_ca2015_all_csv_v3.txt
+	+ sb_ca2015entities_csv.txt
+	+ sb_ca2016_all_csv_v3.txt
+	+ sb_ca2016entities_csv.txt
+	+ sb_ca2017_all_csv_v3.txt
+	+ sb_ca2017entities_csv.txt
 	+ sb_ca2018_all_csv_v3.txt
 	+ sb_ca2018entities_csv.txt
 	+ Subgroups.txt
 * Oregon
+	+ pagr_schools_ela_raceethnicity_1415.xlsx
+	+ pagr_schools_ela_raceethnicity_1516.xlsx
+	+ pagr_schools_ela_raceethnicity_1617.xlsx
 	+ pagr_schools_ela_raceethnicity_1718.xlsx
+	+ pagr_schools_ela_tot_ecd_ext_gnd_lep_1415.xlsx
+	+ pagr_schools_ela_tot_ecd_ext_gnd_lep_1516.xlsx
+	+ pagr_schools_ela_tot_ecd_ext_gnd_lep_1617.xlsx
 	+ pagr_schools_ela_tot_ecd_ext_gnd_lep_1718.xlsx
+	+ pagr_schools_math_raceethnicity_1415.xlsx
+	+ pagr_schools_math_raceethnicity_1516.xlsx
+	+ pagr_schools_math_raceethnicity_1617.xlsx
 	+ pagr_schools_math_raceethnicity_1718.xlsx
+	+ pagr_schools_math_tot_ecd_ext_gnd_lep_1415.xlsx
+	+ pagr_schools_math_tot_ecd_ext_gnd_lep_1516.xlsx
+	+ pagr_schools_math_tot_ecd_ext_gnd_lep_1617.xlsx
 	+ pagr_schools_math_tot_ecd_ext_gnd_lep_1718.xlsx
 * Washington
+	+ Report_Card_Assessment_Data_2014-15_School_Year.csv
+	+ Report_Card_Assessment_Data_2015-16_School_Year.csv
+	+ Report_Card_Assessment_Data_2016-17_School_Year.csv
 	+ Report_Card_Assessment_Data_2017-18_School_Year.csv
 * Common Core of Data 
+	+ ccd_sch_129_1415_w_0216161a.txt
+	+ ccd_sch_129_1516_w_2a_011717.csv
+	+ ccd_sch_129_1617_w_1a_11212017.csv
 	+ ccd_sch_029_1718_w_0a_03302018.csv
 * EDGE (geographies)
 	+ EDGE_GEOCODE_PUBLICSCH_1617.xlsx
--- scripts/dataprep.R
+++ scripts/dataprep.R
@@ -1,24 +1,25 @@
 library(tidyverse)
+library(stringr)
 library(fs)
 library(rio)
 library(pracma)
 library(here)
 library(janitor)
-theme_set(theme_minimal(15))
 
 wa_files <- dir_ls(here("data", "wa_2018"))
 or_files <- dir_ls(here("data", "or_2018"))
 ca_files <- dir_ls(here("data", "ca_2018"))
 
 ### Oregon Prep
-or <- map_df(or_files, import, setclass = "tbl_df", # nrow(or) = 55748
+or <- map_df(or_files, import, setclass = "tbl_df", 
              .id = "file", 
              na = c("-", "--", "*")) %>%
   clean_names() %>%
   mutate(content = gsub(".+_(ela|math)_.+", "\\1", file),
-         content = ifelse(content == "ela", "ELA", "Math")) %>%
-  select(district, district_id, school, school_id, content, student_group,
-         grade_level, 
+         content = ifelse(content == "ela", "ELA", "Math"),
+         year = gsub(".+_(\\d\\d\\d\\d)\\.xlsx", "\\1", file)) %>%
+  select(district, district_id, school, school_id, content, year, 
+         student_group, grade_level, 
          number_level_1, number_level_2, number_level_3, number_level_4)
 
 or <- or %>% 
@@ -39,17 +40,21 @@ or <- or %>%
             TRUE ~ student_group))
 
 or <- or %>%
-  arrange(district_id, school_id, content, grade, student_group,
+  arrange(year, district_id, school_id, content, grade, student_group,
           performance_level) %>%
-  group_by(district_id, school_id, content, grade, student_group) %>%
+  group_by(year, district_id, school_id, content, grade, student_group) %>%
   add_count(wt = number) %>%
   ungroup() %>%
   mutate(percent = (number/n)*100) %>%
   select(-number)
 
 ### Washington Prep
-wa <- import(wa_files, setclass = "tbl_df") %>% 
+wa <- map_df(wa_files, import, setclass = "tbl_df", 
+             .id = "file", 
+             na = c("-", "--", "*")) %>%
   clean_names() %>%
+  mutate(year = gsub(".+_(\\d\\d\\d\\d-\\d\\d)_.+", "\\1", file),
+         year = gsub("20|-", "", year)) %>%
   filter(test_administration_group == "General") %>%
   rename(district = district_name,
          district_id = district_code,
@@ -58,9 +63,9 @@ wa <- import(wa_files, setclass = "tbl_df") %>%
          grade = grade_level,
          content = test_subject,
          n = count_of_students_expected_to_test_including_previously_passed) %>%
-  select(district, district_id, school, school_id, content, student_group,
-         grade, n, percent_level1, percent_level2, percent_level3, 
-         percent_level4) %>%
+  select(district, district_id, school, school_id, content, year, 
+         student_group, grade, n, 
+         percent_level1, percent_level2, percent_level3, percent_level4) %>%
   filter(content != "Science")
 
 wa <- wa %>%  
@@ -82,16 +87,22 @@ wa <- wa %>%
          content = ifelse(content == "English Language Arts", 
                           "ELA", 
                           content)) %>%
-  arrange(district_id, school_id, content, grade, student_group,
+  arrange(year, district_id, school_id, content, grade, student_group,
           performance_level) %>%
   drop_na(percent, school_id) %>%
   filter(!(grepl("Total", school) | grepl("Total", district)))
 
 ### California Prep
-ca <- import(ca_files[grep("all", ca_files)], 
-             setclass = "tbl_df") %>%
+ca <- map_df(ca_files[grep("all", ca_files)], read_csv,
+             .id = "file", 
+             na = c("-", "--", "*"),
+             col_types = cols(.default = "c")) %>%
   clean_names() %>%
-  mutate(content = ifelse(test_id == 1, "ELA", "Math")) %>%
+  filter(school_code != "0000000") %>% 
+  mutate(year = gsub(".+ca(\\d\\d\\d\\d).+", "\\1", file),
+         year = gsub("20", "", year),
+         year = paste0(as.numeric(year) - 1, year),
+         content = ifelse(test_id == 1, "ELA", "Math")) %>%
   rename(district_id = district_code,
          school_id = school_code,
          n = students_with_scores,
@@ -99,39 +110,52 @@ ca <- import(ca_files[grep("all", ca_files)],
          p2 = percentage_standard_nearly_met,
          p3 = percentage_standard_met,
          p4 = percentage_standard_exceeded) %>%
-  select(district_id, school_id, content, subgroup_id, grade, 
+  select(district_id, school_id, content, year, subgroup_id, grade, 
          n, p1, p2, p3, p4)
 
 ca <- ca %>%
   gather(performance_level, percent, matches("^p\\d")) %>%
   mutate(performance_level = parse_number(performance_level), 
          percent = ifelse(percent == "*", NA, percent),
-         percent = parse_number(percent)) %>%
-  filter(school_id != 0)
+         percent = parse_number(percent),
+         grade = parse_number(grade),
+         n = parse_number(n)) %>%
+  filter(as.numeric(school_id) > 0) %>%
+  drop_na(percent)
 
-ca_entities <- import(ca_files[grep("entities", ca_files)], 
-                      setclass = "tbl_df") %>%
+ca_entities <- map_df(ca_files[grep("entities", ca_files)], read_csv,
+             .id = "file", 
+             na = c("-", "--", "*"),
+             col_types = cols(.default = "c")) %>%
   clean_names() %>%
+  mutate(year = gsub(".+ca(\\d\\d\\d\\d).+", "\\1", file),
+         year = gsub("20", "", year),
+         year = paste0(as.numeric(year) - 1, year)) %>%
   rename(district_id = district_code,
          school_id = school_code,
          district = district_name, 
          school = school_name) %>%
-  select(district_id, district, school_id, school) %>%
-  filter(school_id != 0)
+  select(district_id, district, school_id, school, year) %>%
+  filter(as.numeric(school_id) > 0)
 
 ca <- left_join(ca, ca_entities)
 
-ca_subgroups <- import(ca_files[grep("Subgroups", ca_files)], 
-                       setclass = "tbl_df") %>%
-  setNames(c("code_string", "subgroup_id", "student_group", "variable")) %>%
+ca_subgroups <- map_df(ca_files[grep("Subgroups", ca_files)], read_csv,
+             .id = "file", 
+             na = c("-", "--", "*"),
+             col_types = cols(.default = "c"),
+             col_names = FALSE) %>%
+  clean_names()  %>%
+  setNames(c("file", "code_string", "subgroup_id", 
+             "student_group", "variable")) %>%
   select(subgroup_id, student_group, variable)
 
 ca <- left_join(ca, ca_subgroups) %>%
   filter(variable == "Ethnicity"| 
            variable == "Economic Status" |
            variable == "All Students") %>%
-  select(district, district_id, school, school_id, content, student_group,
-         grade, performance_level, n, percent) %>%
+  select(district, district_id, school, school_id, content, year,
+         student_group, grade, performance_level, n, percent) %>%
   filter(!is.na(percent),
          student_group == "Hispanic or Latino"|
            student_group == "White" |
@@ -141,8 +165,10 @@ ca <- left_join(ca, ca_subgroups) %>%
            case_when(student_group == "Hispanic or Latino" ~ "Hispanic",
                      student_group == "Economically disadvantaged" ~ "Econ Dis",
                      student_group == "All Students" ~ "Total Pop",
-                     TRUE ~ student_group),
-           n = as.numeric(n)) 
+                     TRUE ~ student_group)) %>%
+  mutate_at(vars(district_id, school_id), list(as.double)) %>% 
+  filter(n != 0)
+
 
 ### Bind together
 d <- bind_rows(ca, or, wa, .id = "state") %>%
@@ -150,14 +176,16 @@ d <- bind_rows(ca, or, wa, .id = "state") %>%
                            state == 2 ~ "OR",
                            state == 3 ~ "WA"),
          prop = percent/100) %>%
-  select(-percent)
+  select(-percent) %>% 
+  filter(n > 5)
+
 
 # Prep for auc
 eth <- d %>%
   filter(student_group == "Hispanic" | student_group == "White") %>%
-  arrange(state, district_id, school_id, content, grade, student_group,
+  arrange(state, year, district_id, school_id, content, grade, student_group,
           performance_level) %>%
-  group_by(state, district_id, school_id, content, grade, student_group) %>%
+  group_by(state, year, district_id, school_id, content, grade, student_group) %>%
   mutate(cum_prop = cumsum(prop)) %>%
   select(-prop, -n) %>%
   spread(performance_level, cum_prop) %>% 
@@ -169,8 +197,8 @@ eth <- d %>%
 
 econ_school_select <- d %>% # Restrict to schools that have econ dis & all students reported
   filter(student_group == "Econ Dis" | student_group == "Total Pop") %>%
-  distinct(state, district_id, school_id, content, grade, student_group) %>%
-  group_by(state, district_id, school_id, content, grade) %>%
+  distinct(state, year, district_id, school_id, content, grade, student_group) %>%
+  group_by(state, year, district_id, school_id, content, grade) %>%
   count() %>%
   filter(n == 2) %>%
   select(-n) %>%
@@ -178,16 +206,16 @@ econ_school_select <- d %>% # Restrict to schools that have econ dis & all stude
 
 econ_dis <- semi_join(d, econ_school_select) %>%
   filter(student_group == "Econ Dis" | student_group == "Total Pop") %>%
-  arrange(state, district_id, school_id, content, grade, student_group,
+  arrange(state, year, district_id, school_id, content, grade, student_group,
           performance_level) %>%
-  group_by(state, district_id, school_id, content, grade, student_group) %>%
+  group_by(state, year, district_id, school_id, content, grade, student_group) %>%
   mutate(cum_prop = cumsum(prop)) %>%
   select(-prop, -n) %>%
   spread(performance_level, cum_prop) %>%
   mutate(`0` = 0) %>%
   gather(performance_level, cum_prop, `1`:`0`, convert = TRUE) %>%
   spread(student_group, cum_prop) %>%
-  arrange(state, district_id, school_id, content, grade, performance_level) %>%
+  arrange(state, year, district_id, school_id, content, grade, performance_level) %>%
   ungroup()
 
 # Calculate effect sizes	
@@ -200,13 +228,13 @@ v <- function(x, y) {
 }
 
 hisp_gaps <- eth %>%
-  group_by(state, district_id, school_id, content, grade) %>%
+  group_by(state, year, district_id, school_id, content, grade) %>%
   summarize(v_hisp = v(Hispanic, White)) %>%
   drop_na(v_hisp, district_id) %>%
   ungroup()
 
 econ_gaps <- econ_dis %>%
-  group_by(state, district_id, school_id, content, grade) %>%
+  group_by(state, year, district_id, school_id, content, grade) %>%
   summarize(v_econ = v(`Econ Dis`, `Total Pop`)) %>%
   drop_na(v_econ, district_id) %>%
   ungroup()
@@ -220,48 +248,125 @@ length(unique(gaps$school_id[!is.na(gaps$v_hisp)]))
 length(unique(gaps$school_id[!is.na(gaps$v_econ)]))
 
 ### Add in geocoded data
-ccd <- read_csv(here("data", "geographic", 
-                     "ccd_sch_029_1718_w_0a_03302018.csv")) %>%
-  clean_names() %>% 
-  rename(school_id = schid)
+ccd_files <- dir_ls(here("data", "geographic"))
 
-ccd <- ccd %>% 
-  filter(st %in% c("CA", "OR", "WA")) %>% 
-  select(sch_name, st_schid, ncessch) %>% 
-  separate(st_schid, c("state", "district_id", "school_id"), sep = "-") %>% 
-  mutate(school_id = parse_number(school_id)) %>% 
-  select(-district_id)
+ccd15 <- read_delim(ccd_files[grep("\\.txt", ccd_files)], delim = "\t") %>%
+  clean_names() %>%
+  filter(stabr %in% c("CA", "OR", "WA")) %>%
+  mutate(year = gsub("20|-", "", survyear)) %>%
+  select(school_id = st_schid, state = stabr, year, ncessch)
 
-gaps <- left_join(gaps, ccd)
+
+ccd16 <- read_csv(ccd_files[grep("1516", ccd_files)]) %>%
+  clean_names() %>%
+  filter(stabr %in% c("CA", "OR", "WA")) %>%
+  mutate(year = gsub("20|-", "", survyear)) %>%
+  select(school_id = st_schid, state = stabr, year, ncessch) %>%
+  separate(school_id, c(NA, "school_id"), sep = "-")
+
+ccd17 <- read_csv(ccd_files[grep("1617_", ccd_files)]) %>%
+  clean_names() %>%
+  filter(st %in% c("CA", "OR", "WA")) %>%
+  mutate(year = gsub("20|-", "", school_year)) %>%
+  select(school_id = st_schid, state = st, year, ncessch) %>%
+  separate(school_id, c(NA, NA, "school_id"), sep = "-")
+
+ccd18 <- read_csv(ccd_files[grep("1718_", ccd_files)]) %>%
+  clean_names() %>%
+  filter(st %in% c("CA", "OR", "WA")) %>%
+  mutate(year = gsub("20|-", "", school_year)) %>%
+  select(school_id = st_schid, state = st, year, ncessch) %>%
+  separate(school_id, c(NA, NA, "school_id"), sep = "-")
+
+ccd <- bind_rows(ccd15, ccd16, ccd17, ccd18)
+
+ccd %>%
+  count(state, nchar(school_id))
+
+gaps <- gaps %>%
+  mutate(school_id = case_when(state == "CA" ~ 
+                                 str_pad(school_id, 7, "left", pad = "0"),
+                               state == "OR" ~ 
+                                 str_pad(school_id, 20, "left", pad = "0"),
+                               state == "WA" ~ 
+                                 str_pad(school_id, 4, "left", pad = "0"))
+  )
+
+gaps <- left_join(gaps, ccd) 
+table(is.na(gaps$ncessch))
 
 edge <- import(here("data", "geographic",
                          "EDGE_GEOCODE_PUBLICSCH_1617.xlsx"),
                     setclass = "tbl_df") %>% 
-  clean_names()
+  clean_names() %>%
+  select(state, ncessch, lat, lon, city, cnty, nmcnty, cbsatype, cbsa, nmcbsa) %>% 
+  mutate(cbsatype = case_when(cbsatype == 1 ~ "Metropolitan",
+                              cbsatype == 2 ~ "Micropolitan", 
+                              TRUE ~ "Non-CBSA"))
 
-gaps <- left_join(gaps, edge, by = c("state", "ncessch")) %>%
-  select(state, district_id, school_id, sch_name, ncessch, 
-         city, cnty, nmcnty, lat, lon, content, grade, v_hisp, v_econ)
+gaps <- left_join(gaps, edge) %>%
+  select(year, state, district_id, school_id, ncessch, 
+         content, grade, lat, lon,city, cnty, nmcnty, cbsatype, cbsa, nmcbsa, 
+         v_hisp, v_econ)
+
+gaps %>%
+  count(year, missing = is.na(lat)) %>%
+  group_by(year) %>%
+  mutate(percent = n/sum(n)) %>%
+  select(-n) %>%
+  spread(missing, percent)
 
 # Add N's back in
 ns <- d %>%
-  select(-performance_level, -prop) %>%
-  group_by(state, district_id, school_id, content, grade) %>%
-  summarize(n = sum(n))
+  select(-performance_level, -prop, -district, -school) %>%
+  filter(student_group == "Total Pop") %>% 
+  distinct(.keep_all = TRUE) %>% 
+  select(-student_group) %>%
+  mutate(school_id = case_when(state == "CA" ~ 
+                                 str_pad(school_id, 7, "left", pad = "0"),
+                               state == "OR" ~ 
+                                 str_pad(school_id, 20, "left", pad = "0"),
+                               state == "WA" ~ 
+                                 str_pad(school_id, 4, "left", pad = "0"))
+  )
 
 gaps <- left_join(gaps, ns)
+
+# Add cohort variable
+cohorts <- expand.grid(year = c("1415", "1516", "1617", "1718"), 
+                       grade = c(3:8, 11, 13)) %>%
+  arrange(grade, year) %>%
+  mutate(cohort = c(6:3, 7:4, 8:5, 9:6, 10:7, 11:8, 12:9, 13:10),
+         cohort = ifelse(year == "1718", grade, cohort),
+         cohort = case_when(grade == 11 & year == "1617" ~ 12,
+                            grade == 11 & year == "1516" ~ 13,
+                            grade == 11 & year == "1415" ~ 14,
+                            grade == 13 & year == "1617" ~ 14,
+                            grade == 13 & year == "1516" ~ 15,
+                            grade == 13 & year == "1415" ~ 16, 
+                            TRUE ~ as.double(cohort)),
+         year = as.character(year)) 
+
+gaps <- left_join(gaps, cohorts)
+
+# fill in missing lon lat if the school has it in any year for any grade
+gaps <- gaps %>%
+  group_by(school_id) %>%
+  fill(lat)
+
+gaps <- gaps %>%
+  group_by(school_id) %>%
+  fill(lon)
 
 length(unique(gaps$school_id[!is.na(gaps$v_hisp)]))
 length(unique(gaps$school_id[!is.na(gaps$v_econ)]))
 
 # write out csv
 gaps %>% 
-  select(state:grade, n, v_hisp, v_econ) %>% 
-  arrange(state, district_id, school_id, content, grade) %>% 
+  arrange(state, year, district_id, school_id, content, grade) %>% 
 write_csv(here("data", "achievement-gaps-geocoded.csv"))
 
 # write out feather file
 gaps %>% 
-  select(state:grade, n, v_hisp, v_econ) %>% 
-  arrange(state, district_id, school_id, content, grade) %>% 
-  feather::write_feather(here("data", "achievement-gaps-geocoded.feather"))
+  arrange(state, year, district_id, school_id, content, grade) %>% 
+feather::write_feather(here("data", "achievement-gaps-geocoded.feather"))
